00bf04c12d2260cdccdc02844b76f1fec84c0a6e
Lucas-O-S <lucasoscomercial@gmail.com>
2024-11-09 15:11:24 -0300

    Correção

--- FotoViagem/FotoViagem/obj/Debug/net8.0/FotoViagem.AssemblyInfo.cs
+++ FotoViagem/FotoViagem/obj/Debug/net8.0/FotoViagem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FotoViagem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e97265724c6b7bf9c39a5ed8536bdc80e8566052")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+835ccc6b32feae62983e1d804f768aa7493e2683")]
 [assembly: System.Reflection.AssemblyProductAttribute("FotoViagem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FotoViagem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
